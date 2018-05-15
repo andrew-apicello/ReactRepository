160b52cfe92979be7e80acff88a4661ea547ed33
Andrew C. Apicello <andrew.apicello@gmail.com>
2018-05-15 11:25:12 -0400

    end of section 4

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,35 +7,47 @@ class App extends Component {
 
   state = {
     persons: [
-      {name:"Max", age: 29},
-      {name:"Andrew", age: 27},
-      {name: "John", age: 28}
-    ]
+      {id: '15343', name:"Max", age: 29},
+      {id: '25345', name:"Andrew", age: 27},
+      {id: '64243', name: "John", age: 28}
+    ],
+    showPeople: false
   }
 
 
-  switchNameHandler = (newName) => {
-    console.log("Click Successful")
-    this.setState({
-      persons: [
-      {name:newName, age: 29},
-      {name:"Andrew", age: 27},
-      {name: "John", age: 28}
-    ]
-  })
-  }
+ 
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, key) => {
   	console.log("On Change Successful")
-    this.setState({
-      persons: [
-      {name:"Max", age: 29},
-      {name: event.target.value, age: 27},
-      {name: "John", age: 28}
-    ]
-  })
+  	const personIndex = this.state.persons.findIndex(p => {
+  		return p.id === key;
+  	})
+    
+    const person = {
+    	...this.state.persons[personIndex]
+    };
 
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons:persons})
   }
+
+
+	deleteNameHandler = (personsIndex) => {
+		console.log("Name Successfully Deleted");
+		const persons = [...this.state.persons];
+		persons.splice(personsIndex,1);
+		this.setState({persons:persons})
+	}
+
+    togglePersonHandler = () => {
+    	const doesShow = this.state.showPeople;
+    	this.setState({showPeople: !doesShow});
+    }
+
+  
 
   render() {
 
@@ -48,24 +60,33 @@ class App extends Component {
 	}
 
 
+	let persons = null;
+
+	if(this.state.showPeople === true){
+		persons = (
+			<div>
+			{this.state.persons.map((person, index) => {
+				return <Person
+					name={person.name}
+					age={person.age}
+					click={this.deleteNameHandler.bind(this, index)}
+					key={person.id}
+					change={(event)=> this.nameChangedHandler(event, person.id)}
+					/>
+			})}
+        	</div>
+		);
+	}
+
+
+
     return (
       <div className="App">
-        <h1 className="App-title">Hi my name is Andrew</h1>
-        <button 
-        	style={style}
-        	onClick={this.switchNameHandler.bind(this, "Maximillian")}>Switch Name</button>
-        <Person 
-        	name={this.state.persons[0].name} 
-        	age={this.state.persons[0].age}/>
-        <Person 
-        	name={this.state.persons[1].name} 
-        	age={this.state.persons[1].age}
-        	click={this.switchNameHandler.bind(this, "Max!")}
-        	change={this.nameChangedHandler}
-        	>My Hobbies: Racing</Person>
-        <Person 
-        	name={this.state.persons[2].name} 
-        	age={this.state.persons[2].age}/>
+        <h1 className="App-title">Hi, my name is Andrew</h1>
+        	<button 
+        	onClick={this.togglePersonHandler}>Toggle People
+        	</button>
+     	{persons}
       </div>
     );
   }
